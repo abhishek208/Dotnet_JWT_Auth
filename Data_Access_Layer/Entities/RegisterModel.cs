56b4630074448a7a66d8f91656e163fdb2b56fd5
Abhishek Singh <ssabhishek070@gmail.com>
2022-10-10 12:24:18 +0530

    initial

---
+++ Data_Access_Layer/Entities/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Entities
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = " User Name is required ")]
+        public string userName { get; set; }
+        [Required(ErrorMessage = " Email is required ")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = " password is required ")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Data_Access_Layer/Entities/UserRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Entities
+{
+    public static class UserRoles
+    {
+        public const string User = "User";
+        public const string Admin = "Admin";
+    }
+}
